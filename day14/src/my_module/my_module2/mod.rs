46cc3cfd9c0d2956ff8e09b42dc71ad2177af0da
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2023-02-25 11:30:22 -0500

    update-automated-save

--- day14/src/my_module/my_module2/mod.rs
+++ day14/src/my_module/my_module2/mod.rs
@@ -1,1 +1,23 @@
+
 pub mod submodule2;
+
+use std::sync::{Arc, Mutex};
+use lazy_static::lazy_static;
+
+// Define a global vector to store the integers
+lazy_static! {
+    static ref INTEGER_VECTOR: Arc<Mutex<Vec<i32>>> = Arc::new(Mutex::new(Vec::new()));
+}
+
+// Add an integer to the global vector
+#[allow(dead_code)]
+pub fn add_integer(integer: i32) {
+    let mut integer_vector = INTEGER_VECTOR.lock().unwrap();
+    integer_vector.push(integer);
+}
+
+// Get a reference to the global vector
+#[allow(dead_code)]
+pub fn get_integer_vector() -> Arc<Mutex<Vec<i32>>> {
+    Arc::clone(&INTEGER_VECTOR)
+}
--- day14/src/my_module/my_module2/submodule2.rs
+++ day14/src/my_module/my_module2/submodule2.rs
@@ -1,5 +1,7 @@
+
 #[allow(dead_code)]
 pub fn add(x: i32, y: i32) -> i32 {
     println!("in add submodule2");
     x + y
 }
+
